bf8fca11675dbbe1dfebb8c924466e1092243805
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-08-10 21:46:11 -0400

    remove console.log user stuff

--- models/User.js
+++ models/User.js
@@ -50,10 +50,8 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
  */
 userSchema.methods.gravatar = function(size) {
   if (!size) size = 200;
-  console.log(this)
   if (!this.email) return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
   var md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  console.log('gravatar md5 = ' + md5);
   return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
 };
 
