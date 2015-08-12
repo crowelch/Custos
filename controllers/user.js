8aaf10f27a9c4842228eecd5b7c0a328b82ba20b
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-08-12 12:01:03 -0400

    moved helpers again

--- controllers/user.js
+++ controllers/user.js
@@ -26,12 +26,6 @@ exports.getLogin = function (req, res) {
  */
  
 exports.getUserManagement = function (req, res) {
-	var Handlebars = require('express-handlebars');
-	Handlebars.registerHelper('select', function (value, options) {
-		var $el = $('<select />').html(options.fn(this));
-		$el.find('[value="' + value + '"]').attr({ 'selected': 'selected' });
-		return $el.html();
-	});
 	var usersCollection;
 	getCollection("users", function (err, userCollection) {
 		if (err) { return console.log(err); }
