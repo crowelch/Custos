9ed2b6ef7e88de15dd2ab467f17c7083462810fa
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-08-12 11:36:29 -0400

    REMOVED HELPERS

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,2 @@
 $(document).ready(function() {
-	window.Handlebars.registerHelper('select', function (value, options) {
-		var $el = $('<select />').html(options.fn(this));
-		$el.find('[value="' + value + '"]').attr({ 'selected': 'selected' });
-		return $el.html();
-	});
 });
